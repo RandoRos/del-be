46d2eb001da9d4f7e8e50ebda9102a473a3ecf50
Rando Rostok <rrostok@malwarebytes.com>
2022-11-02 21:01:11 +0200

    fixed unpaid query, added pay functionality

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { Op } = require('sequelize');
 
 const {sequelize} = require('./model');
 const {getProfile} = require('./middleware/getProfile');
+const {getAllUnpaid} = require('./queries');
 
 const app = express();
 
@@ -54,29 +55,53 @@ app.get('/contracts', getProfile, async (req, res) => {
  * @returns all active jobs
  */
 app.get('/jobs/unpaid', getProfile, async (req, res) => {
-  const { Contract, Job } = req.app.get('models');
+  const result = await getAllUnpaid(req.profile.id);
+  res.json(result);
+});
 
-  Contract.findAll({
-    attributes: [],
-    where: {
-      status: {
-        [Op.ne]: 'terminated',
+app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
+  const {job_id: jobId} = req.params;
+  const { Profile, Job } = req.app.get('models');
+
+  const jobs = await getAllUnpaid(req.profile.id);
+  const job = jobs.find(j => j.id === Number(jobId));
+
+  if (!job) {
+    res.status(404).end();
+  }
+
+  if (req.profile.balance < job.price) {
+    res.status(201).send('Not enough money to pay');
+  }
+
+  let transaction;
+  try {
+    transaction = await sequelize.transaction();
+    await Profile.update({
+      balance: req.profile.balance - job.price,
+    }, { 
+      where: {
+        id: req.profile.id,
       },
-      [Op.or]: [
-        { ContractorId: req.profile.id },
-        { ClientId: req.profile.id },
-      ],
-    },
-    include: [
-      { model: Job },
-    ],
-  }).then(result => {
-    res.json(
-      result
-        .filter(e => e.Jobs.length)
-        .reduce((acc, item) => acc.concat(item.Jobs), []),
-    );
-  });
+      transaction,
+    });
+    
+    await Job.update({
+      paid: true,
+    }, { 
+      where: {
+        id: jobId,
+      },
+      transaction,
+    });
+
+    await transaction.commit();
+  } catch (err) {
+    res.status(500).send(err);
+    transaction.rollback();
+  }
+
+  res.send(job);
 });
 
 module.exports = app;

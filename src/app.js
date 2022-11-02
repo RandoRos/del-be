3655d600b200af35895796285e36f18302605d83
Rando Rostok <rrostok@malwarebytes.com>
2022-11-02 23:55:38 +0200

    added more methods & refactoring

--- src/app.js
+++ src/app.js
@@ -61,47 +61,141 @@ app.get('/jobs/unpaid', getProfile, async (req, res) => {
 
 app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
   const {job_id: jobId} = req.params;
-  const { Profile, Job } = req.app.get('models');
+  const { Profile, Job, Contract } = req.app.get('models');
 
-  const jobs = await getAllUnpaid(req.profile.id);
-  const job = jobs.find(j => j.id === Number(jobId));
+  if (req.profile.type === 'client') {
+    const jobs = await getAllUnpaid(req.profile.id);
+    const job = jobs.find(j => j.id === Number(jobId));
+  
+    if (!job) {
+      res.status(404).end();
+    }
+  
+    if (req.profile.balance >= job.price) {
+      let transaction;
+      try {
+        transaction = await sequelize.transaction();
+        const contract = await Contract.findOne({ where: { id: job.ContractId }});
+        const contractor = await Profile.findOne({ where: { id: contract.ContractorId } });
 
-  if (!job) {
-    res.status(404).end();
+        await Profile.update({
+          balance: req.profile.balance - job.price,
+        }, { 
+          where: {
+            id: contract.ClientId,
+          },
+          transaction,
+        });
+  
+        await Profile.update({
+          balance: contractor + job.price,
+        }, { 
+          where: {
+            id: contract.ContractorId,
+          },
+          transaction,
+        });
+        
+        await Job.update({
+          paid: true,
+          paymentDate: new Date(),
+        }, { 
+          where: {
+            id: jobId,
+          },
+          transaction,
+        });
+    
+        await transaction.commit();
+        res.send('Done');
+      } catch (err) {
+        res.status(500).send('Something went wrong!');
+        transaction.rollback();
+      }
+    } else {
+      res.status(200).send('Not enough money to pay');
+    }
   }
 
-  if (req.profile.balance < job.price) {
-    res.status(201).send('Not enough money to pay');
-  }
+  res.end();
+});
 
-  let transaction;
-  try {
-    transaction = await sequelize.transaction();
+app.post('/balances/deposit/:userId', getProfile, async (req, res) => {
+  const {userId} = req.params;
+  const { Profile, Job, Contract } = req.app.get('models');
+
+  if (req.profile.type === 'client') {
+    const jobs = await Job.findAll({
+      attributes: [
+        [sequelize.fn('sum',sequelize.col('price')), 'total'],
+      ],
+      include: [
+        {
+          model: Contract,
+          attributes: [],
+          where: {
+            clientId: req.profile.id,
+          },
+        },
+      ],
+      where: {
+        paid: null,
+      },
+      raw: true,
+    });
+
     await Profile.update({
-      balance: req.profile.balance - job.price,
-    }, { 
+      balance: req.profile.balance + Math.floor(jobs[0].total * 0.25),
+    }, {
       where: {
         id: req.profile.id,
       },
-      transaction,
-    });
-    
-    await Job.update({
-      paid: true,
-    }, { 
-      where: {
-        id: jobId,
-      },
-      transaction,
     });
 
-    await transaction.commit();
-  } catch (err) {
-    res.status(500).send(err);
-    transaction.rollback();
+    res.send(req.profile);
   }
 
-  res.send(job);
+  res.end();
+});
+
+app.get('/admin/best-profession', async (req, res) => {
+  const { start, end } = req.query;
+  const { Profile, Job, Contract } = req.app.get('models');
+
+  try {
+    const result = await Contract.findAll({
+      attributes: [
+        'ContractorId',
+        [sequelize.fn('sum',sequelize.col('price')), 'total'],
+      ],
+      include: [
+        { 
+          model: Job,
+          where: { 
+            [Op.and]: {
+              paid: true,
+              createdAt: {
+                [Op.gt]: new Date(start),
+                [Op.lt]: new Date(end),
+              },
+            },
+          },
+          attributes: [],
+        },
+      ],
+      group: [
+        'ContractorId',
+      ],
+      order: [
+        ['total', 'DESC'],
+      ],
+    });
+
+    const top = await Profile.findOne({ where: { id: result[0].ContractorId }});
+    res.send(top.profession);
+  } catch (err) {
+    res.send({});
+  }
 });
 
 module.exports = app;
